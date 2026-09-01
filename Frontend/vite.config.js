38564f0b8088197730d22c2807a34b36fec34613
singhujjwalsingh <singhsingh144ss@gmail.com>
2026-09-01 16:46:47 +0530

    Switch local host to Backend Deployed link

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,9 +7,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://skillsync-eify.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true,
+      },
+      '/uploads': {
+        target: 'https://skillsync-eify.onrender.com',
+        changeOrigin: true,
+        secure: true,
       }
     }
   }
